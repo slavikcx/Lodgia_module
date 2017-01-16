624ec2296ed5acc9ce7906e59d915825a8587848
slavikcx <slavikcx@gmail.com>
2017-01-16 16:15:23 +0200

    DHT library change to AdaFruit, esp unexpected crash resolved. PIR sensor handling added with displaying on web inteface

--- ESP8266_Lodjia_Module.ino
+++ ESP8266_Lodjia_Module.ino
@@ -1,27 +1,26 @@
 //define Libraries
 
 
+#include <DHT_U.h>
+#include <DHT.h>
+#include <Adafruit_Sensor.h>
 #include <ESP8266WiFi.h>
-
 #include <ESP8266WebServer.h> //HTTP WebServer
-#include "PietteTech_DHT.h" //DHT sensor lib
 
 //-------------------------------
-// defines for DHT lib
-#define DHTTYPE  DHT11           // Sensor type DHT11/21/22/AM2301/AM2302
-#define DHTPIN   4              // Digital pin for communications
-#define REPORT_INTERVAL 10000    // in msec must > 2000
 // The port to listen for incoming TCP connections
 #define LISTEN_PORT           8080 // WiFI Server listen port
-
+// defines for DHT lib
+#define DHTTYPE DHT11
+#define DHTPIN  4
 //-------------------------------
 //define constants
 // WiFi parameters
-//const char* ssid = "Delphi_Guests";
-//const char* password = "tofave12";
+const char* ssid = "Delphi_Guests";
+const char* password = "tofave12";
 
-const char* ssid = "SlavaWIFI";
-const char* password = "ZXCde3_vgy765_1234567890_vvv";
+//const char* ssid = "SlavaWIFI";
+//const char* password = "ZXCde3_vgy765_1234567890_vvv";
 
 //Time server 
 const char* timeServerHost = "ntp.time.in.ua";
@@ -31,16 +30,19 @@ const int timeServerHttpPort = 13;
 //-------------------------------
 //define globals
 
-// for DHT
-unsigned long startMills;
-float t, h, d;
-int acquireresult;
-int acquirestatus;
-bool bDHTstarted;       // flag to indicate we started acquisition
-int thAverageVar = 0; //divider for get average T and H during period of time
-float tempT, tempH, aT, aH = 0;
+// for DHT and getTemperature
+//DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
+DHT_Unified dht(DHTPIN, DHTTYPE);
+float insideHumidity, insideTemperature;  // Values read from sensor
+unsigned long previousMillis = 0;        // last measuring time
+const long THinterval = 5000;              // interval at which to read sensor
 
+//for DateTime
+unsigned long previousTimeMillis = 0; //last connection to time server time
+const long DTinterval = 1000; //time synch interval
 String dateTime = "";
+
+String UpTime = "";
 String webPage = "";
 String xml = "";
 
@@ -54,6 +56,8 @@ int maxTranzLevel = 1023; //Maximim level for opening Tranzistor (213 - 4.2V on 
 
 bool pirState = false;
 
+//int cycle=0;
+
 //Define pins
 int lightPin = 5; //light pin
 int powerPin = 13; //power pin 13
@@ -63,9 +67,6 @@ int pirPin = 0; //PIR sensor pin
 //------------------------------
 //Functions declaration
 
-//Libs functions
-void dht_wrapper(); // must be declared before the lib initialization
-
 // Init functions
 void dhtInit ();
 void webServerInit(void);
@@ -74,8 +75,13 @@ void WiFiInit();
 //User functions
 void getTemperature();
 String getTime();
+String moduleUpTime();
 //String toString (float data);
+
+//for Web Interface
 String buildWebPage();
+void handle_xml();
+String buildXML();
 void handle_root();
 //functions for light control
 void handle_lightOn();
@@ -84,14 +90,13 @@ void handle_lightOff();
 void handle_powerOn();
 void handle_powerOff();
 
-void handle_xml();
-String buildXML();
-
+//for motion detection
 void detectMotion();
+
 
 //------------------------------
 // Libs instantiate
-PietteTech_DHT DHT(DHTPIN, DHTTYPE, dht_wrapper);
+
 // Create an instance of the server
 WiFiServer server(LISTEN_PORT);
 //Creating webServer instance with listening of port 80
@@ -102,9 +107,8 @@ ESP8266WebServer webServer(80);
 // Initialize the system
 void setup()
 {
-
   Serial.begin(115200); 
-  Serial.setDebugOutput(true); // Uncomment if debug serial messages needed
+  //Serial.setDebugOutput(true); // Uncomment if debug serial messages needed
   Serial.println("");
   Serial.println("Setup Started....");
   Serial.println ("-------------------------");
@@ -115,8 +119,9 @@ void setup()
   // init and start Web server
   webServerInit();
   //Initializing DHT
-  dhtInit ();
+  dhtInit();
 
+  //Setting pins mode and state
   pinMode(lightPin, OUTPUT); // setting light pin to output
   digitalWrite(lightPin, LOW); //Light is OFF by default
 
@@ -133,14 +138,25 @@ void setup()
 
 void loop()
 {
-
-	
+	//Serial.println("START OF LOOP");
+	/*cycle++;
+	Serial.print ("cycle - ");
+	Serial.println(cycle);
+	Serial.println("In Loop before delay 50ms ");
+	delay(50);
+	Serial.println("In Loop after delay 50ms ");*/
     webServer.handleClient();
     delay(1);
+
+	UpTime = moduleUpTime();
 	
+	getTime();
+
+	//Serial.println("In Loop before getTemp");
 	getTemperature();
 
 	detectMotion();
+
 	
 	//Serial.println("before client init");
 
@@ -148,16 +164,32 @@ void loop()
 	
 	//Serial.println("after client init");
 	
-  if (!client) {
-    return;
-  }
-
-  //Serial.println("before client available");
-  while (!client.available()) {
-    delay(1);
-  }
-  
-  
+//
+//	/*while (!client)
+//	{
+//		Serial.println("Client not init");
+//		delay(500);
+//	}
+//	 Serial.println("Client init succeed");
+//*/
+// /* if (!client) {
+//    Serial.println("Client not init");
+//	  return;
+//  } else {
+//	   Serial.println("Client init succeed");
+//  }*/
+//
+// /* Serial.println("before client available");
+//  while (!client.available()) {
+//    Serial.println("Client not available");
+//	  delay(100);
+//  }*/
+//  
+//	//Serial.print("Module UP time - ");
+//	//Serial.print(moduleUpTime());
+//	//Serial.println("");
+//
+//  //Serial.println("END OF LOOP");
 }
 
 void webServerInit(void)
@@ -202,7 +234,7 @@ String buildWebPage()
 	page += "function process(){";
 	page += "if(xmlHttp.readyState==0 || xmlHttp.readyState==4){";
 	page += "xmlHttp.open('PUT','xml',true); xmlHttp.onreadystatechange=handleServerResponse; xmlHttp.send(null);}";
-	page += "setTimeout('process()',5000);}";
+	page += "setTimeout('process()',1000);}";
 
 	page += "function handleServerResponse(){";
 	page += "if(xmlHttp.readyState==4 && xmlHttp.status==200){";
@@ -214,6 +246,8 @@ String buildWebPage()
 	page += "xmldoc = xmlResponse.getElementsByTagName('lightState'); message = xmldoc[0].firstChild.nodeValue;  document.getElementById('lightState').innerHTML = message;";
 	page += "xmldoc = xmlResponse.getElementsByTagName('powerState'); message = xmldoc[0].firstChild.nodeValue;  document.getElementById('powerState').innerHTML = message;";
 	page += "xmldoc = xmlResponse.getElementsByTagName('pirState'); message = xmldoc[0].firstChild.nodeValue;  document.getElementById('pirState').innerHTML = message;";
+	page += "xmldoc = xmlResponse.getElementsByTagName('UpTime'); message = xmldoc[0].firstChild.nodeValue;  document.getElementById('UpTime').innerHTML = message;";
+
 
 	page += "}}";
 	
@@ -224,15 +258,13 @@ String buildWebPage()
 	page += "request.send();}";
 
 
-
-
 	page += "</SCRIPT>";
 
 
 	page += "<html> <head> <title>Test page</title> </head>";
 	page += "<body onload='process()'>"; //runnig function when page loaded
 
-	page += "<h1>Test page</h1>";	//page header
+	page += "<h1>Test page                   <span style='font-size:9px'>--UP Time <span id='UpTime'></span>--</span></h1>";	//page header
 
 	page += "<h2 id='dateTime'> </h2>"; //show date and time
 	page += "<h2>Temperature: <span id='temperature'> </span></h2>"; //show temperature
@@ -261,7 +293,10 @@ void handle_root()
 {
 	webPage = buildWebPage();
 	webServer.send(200, "text/html", webPage);
-	//Serial.println(webPage);
+	/*Serial.println("");
+	Serial.print("Web Page sent - ");
+	Serial.println(webPage);
+	Serial.println("");*/
 	
 }
 
@@ -286,13 +321,17 @@ String buildXML()
     xml += "<dateTime>";
 	xml += dateTime;
     xml += "</dateTime>";
+
+	xml += "<UpTime>";
+	xml += UpTime;
+    xml += "</UpTime>";
     
 	xml += "<temperature>";
-	xml += aT;
+	xml += insideTemperature;
 	xml += "</temperature>";
 
 	xml += "<humidity>";
-	xml += aH;
+	xml += insideHumidity;
 	xml += "</humidity>";
 
 	xml += "<lightState>";
@@ -375,106 +414,76 @@ void detectMotion() {
 	}
 	
 }
-// Get temperature function
+
 void getTemperature(){
   
 	//Serial.println("In GetTemperatre");
+	unsigned long currentMillis = millis();
 
-   if (bDHTstarted) {
-    acquirestatus = DHT.acquiring();
-    if (!acquirestatus) {
-      acquireresult = DHT.getStatus();
-      if ( acquireresult == 0 ) {
-		  t = DHT.getCelsius();
-          h = DHT.getHumidity();
-        //d = DHT.getDewPoint();
-      }
-      bDHTstarted = false;
-    }
-  }
-   //Serial.println("After T and H collected from DHT");
+	if (currentMillis - previousMillis >= THinterval) {
+		
+				// save the last time you read the sensor 
+		previousMillis = currentMillis;
 
-   
-   //collecting data for getting average T and H
-   tempT = tempT + t;
-   tempH = tempH + h;
-   thAverageVar ++;
+		// Get temperature event and print its value.
+		sensors_event_t event;  
 
-   //Serial.println("After summation of temp values");
+		 dht.temperature().getEvent(&event);
+		if (isnan(event.temperature)) {
+			Serial.println("Error reading temperature!");
+		}
+		 else {
+			insideTemperature = event.temperature;
+			Serial.print("Temperature: ");
+			Serial.print(event.temperature);
+			Serial.println(" *C");
+		}
+		// Get humidity event and print its value.
+		dht.humidity().getEvent(&event);
 
-  if ((millis() - startMills) > REPORT_INTERVAL) {
+		if (isnan(event.relative_humidity)) {
+			Serial.println("Error reading humidity!");
+		}
+		else {
+			insideHumidity = event.relative_humidity;
+			Serial.print("Humidity: ");
+			Serial.print(event.relative_humidity);
+			Serial.println(" %");
+		}
+	}
 
-	  //getting time
-	  Serial.println("before get time in get Temperature");
-	  getTime();
-	  Serial.println("after get time in get Temperature");
-
-	  //getting avarage T and H
-	  aT = tempT/thAverageVar;
-	  aH = tempH/thAverageVar;
-	  
-	  //converting float to string for Win explorer refresh
-	  //saT = toString(aT);
-	  //Serial.print("after tostring conversion - ");
-	  //Serial.println(saT);
-	  //saH = toString(aH);
-
-	  Serial.print("Amounts of measurement: ");
-	  Serial.println(thAverageVar);
-
-      Serial.print("Humidity (%): ");
-	  Serial.println(aH);
-
-
-      Serial.print("Temperature (oC): ");
-	  Serial.println(aT);
-	
-	  //setting all tem vars to 0
-	  //Serial.println("setting all var to 0 get Temperature");
-      startMills = millis();
-	  tempT = 0;
-	  tempH = 0;
-	  thAverageVar = 0;
-
-	  //Serial.println("before remove lock in get temp");
-    // to remove lock
-    if (acquirestatus == 1) {
-      DHT.reset();
-	  thAverageVar = 0;
-    }
-
-	//Serial.println("before non blocking method in get Temperature");
-    if (!bDHTstarted) {
-      // non blocking method
-      DHT.acquire();
-      bDHTstarted = true;
-    }
-  }
   //Serial.println("exition from get Temperature");
 }
-// to string function 
-String getTime(){
-	
-	
-	WiFiClient client;
-	//String datetime = " ";
-	Serial.print("");
-	Serial.print("connecting to ");
-	Serial.println(timeServerHost);
-	if (!client.connect(timeServerHost, timeServerHttpPort)) {
-		Serial.println("Time server connection failed");
 
-		dateTime = "Time server unavailable";
-		return dateTime;
-	} else {
+String getTime() {
 
-		//Serial.println("SEND REQUEST");
-		client.print("HEAD / HTTP/1.1\r\nAccept: */*\r\nUser-Agent: Mozilla/4.0 (compatible; ESP8266 NodeMcu Lua;)\r\n\r\n");
-	
-		delay(100);
-	
+	unsigned long currentTimeMillis = millis();
 
-		while(client.available())
+	if (currentTimeMillis - previousTimeMillis >= 1000)
+	{
+		previousTimeMillis = currentTimeMillis;
+
+		WiFiClient client;
+		//String datetime = " ";
+		Serial.print("");
+		Serial.print("connecting to ");
+		Serial.println(timeServerHost);
+
+		if (!client.connect(timeServerHost, timeServerHttpPort)) {
+			Serial.println("Time server connection failed");
+
+			dateTime = "Time server unavailable";
+			return dateTime;
+		}
+		else {
+
+			Serial.println("SEND REQUEST");
+			client.print("HEAD / HTTP/1.1\r\nAccept: */*\r\nUser-Agent: Mozilla/4.0 (compatible; ESP8266 NodeMcu Lua;)\r\n\r\n");
+
+			delay(100);
+
+
+			while (client.available())
 			{
 				String line = client.readStringUntil('\r');
 
@@ -487,40 +496,20 @@ String getTime(){
 			}
 		}
 
-	dateTime = "Time server unavailable";
-	return dateTime;
+		dateTime = "Time server unavailable";
+		return dateTime;
+	}
 }
 
 void dhtInit ()
 {
 
-  startMills = millis();
-  
-  // delay 2 sec before start acquire DHT
-  while ( (millis() - startMills) < 2000 ) {
-    yield();
-  }
+	dht.begin();           // initialize temperature sensor
 
-// blocking method for DHT
-  acquirestatus = 0;
-  acquireresult = DHT.acquireAndWait(1000);
-  if ( acquireresult == 0 ) {
-    t = DHT.getCelsius();
-    h = DHT.getHumidity();
-    d = DHT.getDewPoint();
-  } else {
-    t = h = d = 0;
-  }
   Serial.println ("DHT sensor initialization succeed.");
   Serial.println ("");
 }
 
-// This wrapper is in charge of calling must be defined like this for the lib work
-void dht_wrapper() {
-  DHT.isrCallback();
-}
-
-//Init wifi
 void WiFiInit()
 {
 	// tryin got connect to WiFi net
@@ -582,7 +571,22 @@ void WiFiInit()
   Serial.println ("");
 }
 
-
+String moduleUpTime(){
+  String Time="";
+  unsigned long ss;
+  byte mm,hh;
+  ss=millis()/1000;
+  hh=ss/3600;
+  mm=(ss-hh*3600)/60;
+  ss=(ss-hh*3600)-mm*60;
+  if(hh<10)Time+="0";
+  Time+=(String)hh+":";
+  if(mm<10)Time+="0";
+  Time+=(String)mm+":";
+  if(ss<10)Time+="0";
+  Time+=(String)ss;
+  return Time;
+}
 
 
 
