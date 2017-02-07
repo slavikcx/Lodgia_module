41d87e122e0f03158585f8ec07f9adb8ec60bba7
slavikcx <slavikcx@gmail.com>
2017-02-07 13:58:52 +0200

    delete files

--- Debug/Source.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\User\AppData\Local\Temp\VMBuilds\ESP8266_Lodgia_Module\esp8266_generic\Debug\Source.cpp.o: \
- C:\Users\User\AppData\Local\Temp\VMBuilds\ESP8266_Lodgia_Module\esp8266_generic\Debug\Source.cpp
